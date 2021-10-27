00b38a4b1892ac889cdc18c4e7573a2df6906710
brygard14 <bryana.gardner@gmail.com>
2021-10-27 14:43:57 -0700

    I love f13

--- lab.js
+++ lab.js
@@ -154,6 +154,7 @@ if(changeMyMind = true) {
 
 console.log(changeMyMind)
 
+console.log('Hello f13!')
 ////////// PROBLEM 10 //////////
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
